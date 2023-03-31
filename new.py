879e1dcd98b03a639430431d21fd628590e0429e
t-aswath <119417646+t-aswath@users.noreply.github.com>
2023-03-31 13:38:59 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,25 @@
+# importing library
+from flask import Flask
+from flask import request
+import requests as rq
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+@app.route('/',methods =['GET','POST'])
+
+text = request.args.get('input')
+city=input("city : ")
+state=input("state : ")
+
+#creating url and requests instance
+url = "https://search.yahoo.com/search?ei=UTF-&p=weather+"+city+"+"+state
+html = rq.get(url)
+
+# getting raw data
+soup = BeautifulSoup(html.text, 'html.parser')
+temp = soup.find('span', attrs={'class': 'currTemp'})
+_str = soup.find('span', attrs={'class': 'condition'})
+a = soup.find('p', attrs={'class':'txt'})
+print(temp.text)
+print(_str.text)
+print(a.text)
