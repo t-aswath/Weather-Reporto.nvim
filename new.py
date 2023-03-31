3d3611fc9f2ab080949490cd72f8acc4d147eea6
t-aswath <119417646+t-aswath@users.noreply.github.com>
2023-03-31 13:41:42 +0530

    Update new.py

--- new.py
+++ new.py
@@ -7,19 +7,18 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 @app.route('/',methods =['GET','POST'])
 
-text = request.args.get('input')
-city=input("city : ")
-state=input("state : ")
+def weather():
+  text = request.args.get('input')
+  city=input("city : ")
+  state=input("state : ")
 
-#creating url and requests instance
-url = "https://search.yahoo.com/search?ei=UTF-&p=weather+"+city+"+"+state
-html = rq.get(url)
+  #creating url and requests instance
+  url = "https://search.yahoo.com/search?ei=UTF-&p=weather+"+city+"+"+state
+  html = rq.get(url)
 
-# getting raw data
-soup = BeautifulSoup(html.text, 'html.parser')
-temp = soup.find('span', attrs={'class': 'currTemp'})
-_str = soup.find('span', attrs={'class': 'condition'})
-a = soup.find('p', attrs={'class':'txt'})
-print(temp.text)
-print(_str.text)
-print(a.text)
+  # getting raw data
+  soup = BeautifulSoup(html.text, 'html.parser')
+  temp = soup.find('span', attrs={'class': 'currTemp'})
+  _str = soup.find('span', attrs={'class': 'condition'})
+  a = soup.find('p', attrs={'class':'txt'})
+  return [temp,_str,a]
