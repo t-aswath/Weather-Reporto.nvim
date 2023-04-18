8385c6c0d670e3362a95a1108e40d35459c5f560
t-aswath <119417646+t-aswath@users.noreply.github.com>
2023-04-18 19:43:02 +0530

    Add files via upload

--- lua/weather/functions.lua
+++ lua/weather/functions.lua
@@ -1,39 +1,37 @@
 -- Define a function to fetch information from a URL
-local function fetch(city,country)
-  local url = "https://search.yahoo.com/search?p=weather+".. city .. "+" ..country
-  -- Use the system function to make a curl request to the URL
-  local handle = io.popen("curl -s " .. url)
-  local response = handle:read("*a")
-  handle:close()
-  -- Check if the request was successful (non-empty response)
-  if response and response ~= "" then
-    -- Return the response body (HTML content)
-    -- Define a regex pattern to match HTML tags and extract their content
-    local pattern = '<span%s+class="currTemp">(.-)</span>'
-    local p2 = "<span%s+class=\"condition\">(.-)</span>"
+local function fetch(city, country)
+	local url = "https://weather-api-support.vercel.app/?long=80.271758&lat=13.085412"
+	-- Use the system function to make a curl request to the URL
+	local handle = io.popen("curl -s " .. url)
+	local response = handle:read("*a")
+	handle:close()
+	-- Check if the request was successful (non-empty response)
+	if response and response ~= "" then
+		-- Return the response body (HTML content)
+		-- Define a regex pattern to match HTML tags and extract their content
+		local pattern = '"temperature":(.-),"'
+		local p2 = '"weathercode":(.-),"'
 
-    -- Create a table to store the extracted data
-    local parsed_data = {}
+		-- Create a table to store the extracted data
+		local parsed_data = {}
 
-    -- Loop through the response and apply the regex pattern
-    for match in response:gmatch(pattern) do
-      -- Add the extracted content to the parsed_data table
-      parsed_data.temp = match
-    end
-
-    for match in response:gmatch(p2) do
-      -- Add the extracted content to the parsed_data table
-        parsed_data.condition = match
-    end
-
-    return parsed_data
-
-  else
-    print("Failed to fetch data from " .. url)
-    return {temp='NaN',condition='NaN'}
-  end
+		-- Loop through the response and apply the regex pattern
+		for match in response:gmatch(pattern) do
+			-- Add the extracted content to the parsed_data table
+			parsed_data.temp = match
+		end
+		--
+		for match in response:gmatch(p2) do
+			-- Add the extracted content to the parsed_data table
+			parsed_data.condition = match
+		end
+		return parsed_data
+	else
+		print("Failed to fetch data from " .. url)
+		return { temp = "NaN", condition = "NaN" }
+	end
 end
 
-local m = fetch("chennai","india")
+-- local m = fetch("chennai", "india")
 -- Call the fetch function with the URL to fetch data
 return fetch
--- lua/weather/init.lua
+++ lua/weather/init.lua
@@ -1,8 +1,8 @@
 -- Define a function to fetch information from a URLs
 --
-local fetchdata = require ("weather.functions")
+local fetchdata = require("weather.functions")
 --local nerdicons = require("weather.devicons")
-local math = require('math')
+local math = require("math")
 
 -- struct defaults
 -- city : string
@@ -13,44 +13,39 @@ local math = require('math')
 --
 -- struct contenot
 -- condition : string
--- temp 
+-- temp
 
 local defaults = {
-        city = "Amsterdam",
-        country = "Netherlands",
-        celsius = false,
+	city = "Amsterdam",
+	country = "Netherlands",
+	celsius = false,
 }
-
 
 local content = {}
 
 function content.setup(opts)
-        opts = opts or {}
-        for k , v in pairs( defaults ) do
-                if opts[k] == nil then
-                          opts[k]=v
-                end
-        end
-        
-        content.feed = fetchdata(opts.city,opts.country)
-        local arg = content.feed
-        if content.feed[1] then
-            arg.celtemp=tostring(math.floor((5/9)*(tonumber(arg.temp)-32)))
-            -- if celsius is true , it will concatenate celsius temp , else farenheit temp --
-            content.strfeed = ((opts.celsius and arg.celtemp) or arg.temp)  .. ((opts.celsius and "°C ") or "°F ") --.. nerdicons[tonumber(arg.condition)]--.. nerdicons.cond(arg.condition)
-            content.kfeed = tostring(tonumber(arg.temp)+241) .. "K " .. arg.condition --.. nerdicons.weathercode[tonumber(arg.condition)]
-        else
-            content.feed = {
-                celtemp="#E3",
-                temp="#E3",
-            }
-            content.kfeed="#E3"
-            content.strfeed="#E3"
-        end
+	opts = opts or {}
+	for k, v in pairs(defaults) do
+		if opts[k] == nil then
+			opts[k] = v
+		end
+	end
 
+	content.feed = fetchdata(opts.city, opts.country)
+	local arg = content.feed
+	if arg.temp and arg.temp ~= "NaN" then
+		arg.celtemp = tostring(math.floor((5 / 9) * (tonumber(arg.temp) - 32)))
+		-- if celsius is true , it will concatenate celsius temp , else farenheit temp --
+		content.strfeed = ((opts.celsius and arg.celtemp) or arg.temp) .. ((opts.celsius and "°C ") or "°F ") --.. nerdicons[tonumber(arg.condition)]--.. nerdicons.cond(arg.condition)
+		content.kfeed = tostring(tonumber(arg.temp) + 241) .. "K " .. arg.condition --.. nerdicons.weathercode[tonumber(arg.condition)]
+	else
+		content.feed = {
+			celtemp = "#E3",
+			temp = "#E3",
+		}
+		content.kfeed = "#E3"
+		content.strfeed = "#E3"
+	end
 end
 
-
 return content
-
-
