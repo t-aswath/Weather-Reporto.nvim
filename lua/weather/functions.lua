61892d75b8af61078a3dfb6f6bb9574bb9d8670a
t-aswath <119417646+t-aswath@users.noreply.github.com>
2023-04-19 09:22:54 +0530

    Add files via upload

--- lua/weather/functions.lua
+++ lua/weather/functions.lua
@@ -1,9 +1,11 @@
 -- Define a function to fetch information from a URL
 local function fetch(lat, lon)
-        --lat and lon are numbers
-        local mg = function (x) return string.format("%s",x) end
+	--lat and lon are numbers
+	local mg = function(x)
+		return string.format("%s", x)
+	end
 
-	local url = "https://weather-api-support.vercel.app/?long=" .. mg(lon) .. "&lat=" .. mg(lat) ..""
+	local url = "https://weather-api-support.vercel.app/?long=" .. mg(lon) .. "&lat=" .. mg(lat) .. ""
 	-- Use the system function to make a curl request to the URL
 	local handle = io.popen("curl -s " .. url)
 	local response = handle:read("*a")
@@ -12,26 +14,33 @@ local function fetch(lat, lon)
 	if response and response ~= "" then
 		-- Return the response body (HTML content)
 		-- Define a regex pattern to match HTML tags and extract their content
-		local pattern = '"temperature":(.-),"'
+		local p1 = '"temperature":(.-),"'
 		local p2 = '"weathercode":(.-),"'
+		local p3 = '"is_day":(.-),"'
 
 		-- Create a table to store the extracted data
 		local parsed_data = {}
 
 		-- Loop through the response and apply the regex pattern
-		for match in response:gmatch(pattern) do
+		for match in response:gmatch(p1) do
 			-- Add the extracted content to the parsed_data table
 			parsed_data.temp = match
 		end
-		--
+
 		for match in response:gmatch(p2) do
 			-- Add the extracted content to the parsed_data table
 			parsed_data.condition = match
 		end
+
+		for match in response:gmatch(p3) do
+			-- Add the extracted content to the parsed_data table
+			parsed_data.isday = match
+		end
+		print(parsed_data.isday)
 		return parsed_data
 	else
 		print("Failed to fetch data from " .. url)
-		return { temp = " ", condition = " " }
+		return { temp = " ", condition = " ", isday = " " }
 	end
 end
 
